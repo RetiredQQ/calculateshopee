adc58e4b3fde750d334daa2dd4cc78c0c2d946ce
Sky <retiredqq@gmail.com>
2023-06-23 10:57:55 +0800

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -1,27 +1,47 @@
-var total = 0;
 var order = 0;
-function calculate(next){
-	var opts = {
-		method: 'GET',      
-		headers: {}
-	};
-	fetch('https://shopee.com.my/api/v4/order/get_order_list?limit=5&list_type=3&offset='+next, opts).then(function (response) {
-		return response.json();
-	})
-	.then(function (body) {
-		var next_offset = body.data.next_offset;
-		if(next_offset >= 0){
-			for (let [key, value] of Object.entries(body.data.details_list)) {
-				var total_temp = value.info_card.final_total / 100000;
-				total += total_temp;
-				order++;
-	    			console.log(order + ":", "RM " + total_temp + " - ", value.info_card.order_list_cards[0].items[0].name);
-			}
-			calculate(next_offset);
-		} else {
-			console.log('Calculation completed!');
-			console.log('GRAND TOTAL: RM ' + Math.round(total * 100) / 100);
-		}
-	});
+var total = 0;
+
+async function calculate(next, count = 0) {
+  const apiUrl = `https://shopee.com.my/api/v4/order/get_order_list?limit=5&list_type=3&offset=${next}`;
+  const headers = {
+    'Accept': 'application/json',
+  };
+
+  console.log(`Please wait... (${count} second)`);
+  console.log('Calculating expenses...');
+
+  const response = await fetch(apiUrl, {
+    method: 'GET',
+    headers: headers,
+  });
+
+  const body = await response.json();
+  const next_offset = body.data.next_offset;
+
+  if (next_offset >= 0) {
+    for (const [key, value] of Object.entries(body.data.details_list)) {
+      const total_temp = value.info_card.final_total / 100000;
+      total += total_temp;
+      order++;
+      //console.log(order + ":", "RM " + total_temp + " - ", value.info_card.order_list_cards[0].items[0].name);
+      // value.info_card.order_list_cards[0].items_groups
+    }
+    await calculate(next_offset, count + 1);
+  } else {
+    console.log('Calculation completed!');
+    console.log('GRAND TOTAL: RM ' + Math.round(total * 100) / 100);
+  }
 }
-calculate(0);
+
+const startTime = new Date().getTime() / 1000;
+calculate(0, 0);
+
+const intervalId = setInterval(() => {
+  const currentTime = new Date().getTime() / 1000;
+  const runningTime = Math.round(currentTime - startTime);
+  console.log(`Please wait... (${runningTime} second)`);
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(intervalId);
+}, 2000);
